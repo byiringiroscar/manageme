ca56e91870efd7a9dc0b025f7bfa6b4aa2510f08
byiringiroscar <byiringoroscar@gmail.com>
2022-01-06 23:44:51 +0200

    remove twillio activity

--- users/utilis.py
+++ users/utilis.py
@@ -1,14 +1,6 @@
 import requests
 
 
-import os
-from twilio.rest import Client
-
-account_sid = "AC38cd1fd291baf345733f4a85cb167cea"
-auth_token = "3c579f2c10c9575d99b992d8ac1ac844"
-client = Client(account_sid, auth_token)
-
-
 def send_sms(user_code, phone_number):
     data = {
         'recipients': f'{phone_number}',
@@ -19,6 +11,3 @@ def send_sms(user_code, phone_number):
     r = requests.post('https://www.intouchsms.co.rw/api/sendsms/.json', data,
                       auth=('byiringoroscar@gmail.com', 'oscarlewis.O1'))
     print(r.json(), r.status_code)
-
-
-
